96b37acdeace42e09a563994209b1a112da02df6
Vitor Sergio <vitor@Vitors-MacBook-Air.local>
2023-04-07 18:07:40 -0300

    refactor: norminette

--- ft_lstadd_back_bonus.c
+++ ft_lstadd_back_bonus.c
@@ -6,13 +6,13 @@
 /*   By: vsergio <vsergio@student.42.rio>           +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/05/19 11:23:09 by vsergio           #+#    #+#             */
-/*   Updated: 2023/04/07 17:54:09 by vsergio          ###   ########.fr       */
+/*   Updated: 2023/04/07 18:07:20 by vsergio          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void		ft_lstadd_back(t_list **lst, t_list *new_node)
+void	ft_lstadd_back(t_list **lst, t_list *new_node)
 {
 	t_list	*temp;
 
